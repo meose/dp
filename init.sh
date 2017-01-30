2034c18a60418fa1e19c7ea1189da2fd57a30b9e
meoson <snurnitsyn@live.ru>
2017-01-30 23:18:03 +0300

    2.1.11 MVC v1: Python 2.x

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 sudo pkill -9 python
-sudo pkill -9 nginx
-sudo pkill -9 gunicorn
 
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-cd /home/box/web/
-gunicorn -w 4 hello:app -b 0.0.0.0:8080
-cd ..
+sudo rm /etc/gunicorn.d/test-wsgi
+sudo rm /etc/gunicorn.d/test-django
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
--- web/hello.py
+++ web/hello.py
@@ -1,6 +1,6 @@
 def app(environ, start_response):
     data = environ['QUERY_STRING'].split('&')
     for i in range(len(data)):
-    	data[i] += "\n"
+     	data[i] += "\n"
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return iter(data)
