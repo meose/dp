1eae69d46d41516c2e3333c703f0fc990e4d7690
meoson <snurnitsyn@live.ru>
2017-01-30 21:16:40 +0300

    1.9.11 Frontend-Backend Architecture Stable: Python 2.x

--- web/hello.py
+++ web/hello.py
@@ -2,7 +2,5 @@ def app(environ, start_response):
     data = environ['QUERY_STRING'].split('&')
     for i in range(len(data)):
     	data[i] += "\n"
-    	data[i] = bytes(data[i], "utf-8")
-    	print(bytes(data[i]))
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return iter(data)
