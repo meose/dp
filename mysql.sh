4d51568d7630f32f37bd556ddf92c8ad4e5cdc8f
svew <snurnitsyn@live.ru>
2017-01-31 20:12:38 +0000

    2.3.10 DBMS v2 VirtualMachine: Python 2.x

--- mysql.sh
+++ mysql.sh
@@ -2,8 +2,8 @@ sudo /etc/init.d/mysql start
 sudo mysql -uroot -e "CREATE DATABASE IF NOT EXISTS base;"
 #sudo mysql -uroot -e "SELECT User, Host FROM mysql.user;"
 #sudo mysql -uroot -e "SET PASSWORD FOR root@localhost=PASSWORD('');"
-#sudo mysql -uroot -e "DROP USER 'sv@localhost';"
-sudo mysql -uroot -e "CREATE USER IF NOT EXISTS 'sven'@'localhost' IDENTIFIED BY 'qrs797';"
+sudo mysql -uroot -e "DROP USER 'sven'@'localhost';"
+sudo mysql -uroot -e "CREATE USER 'sven'@'localhost' IDENTIFIED BY 'qrs797';"
 sudo mysql -uroot -e "GRANT ALL ON base.* TO 'sven'@'localhost';"
 sudo mysql -uroot -e "FLUSH PRIVILEGES;"
 
--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -3,13 +3,12 @@ from django.db import models, connection
 from django.contrib.auth.models import User
 
 # Create managers of your models here
-class QuestionManager(models.Model):
-	"""docstring for ClassName"""
+class QuestionManager(models.Manager):
 	def new(self):
 		cursor = connection.cursor()
-		cursor.execute("""
+		cursor.execute('''
 			SELECT * FROM base.qa_question order by added_at DESC;
-			""")
+			''')
 		result = []
 		for row in cursor.fetchall():
 			p = self.model(id=row[0], title=row[1], text=row[2], added_at=row[3], rating = row[4], author_id = row[5])
@@ -17,9 +16,9 @@ class QuestionManager(models.Model):
 		return result
 	def popular(self):
 		cursor = connection.cursor()
-		cursor.execute("""
+		cursor.execute('''
 			SELECT * FROM base.qa_question order by rating DESC;
-			""")
+			''')
 		result = []
 		for row in cursor.fetchall():
 			p = self.model(id=row[0], title=row[1], text=row[2], added_at=row[3], rating = row[4], author_id = row[5])
@@ -28,16 +27,16 @@ class QuestionManager(models.Model):
 
 # Create your models here
 class Question(models.Model):
+	objects = QuestionManager()
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	added_at = models.DateTimeField()
-	rating = models.IntegerField()
+	added_at = models.DateTimeField(blank=True, auto_now_add=True)
+	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User, related_name="q_author")
 	likes = models.ManyToManyField(User, related_name="q_likes")
-	object = QuestionManager()
 			
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(blank=True, auto_now_add=True)
 	question = models.ForeignKey(Question, related_name="ansToQ")
 	author = models.ForeignKey(User, related_name="a_author")
